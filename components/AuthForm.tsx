f427338baa95c1057c369c70ad82aecc1e7934b3
khaleelullah <khaleelullah@edvoy.com>
2024-12-07 17:43:52 +0530

    added shadcnUi and form libraries

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -1,8 +1,60 @@
+"use client"
 
+import { z } from "zod"
+import { Input } from "@/components/ui/input"
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form"
+import { Button } from "@/components/ui/button"
+
+import {
+    Form,
+    FormControl,
+    FormDescription,
+    FormField,
+    FormItem,
+    FormLabel,
+    FormMessage,
+} from "@/components/ui/form"
+
+const formSchema = z.object({
+    username: z.string().min(2).max(50),
+})
 type FormType = "sign-in" | "sign-up"
 
-const AuthForm = ({type}:{type:FormType}) => {
-  return <div>AuthForm</div>
+const AuthForm = ({ type }: { type: FormType }) => {
+    const form = useForm<z.infer<typeof formSchema>>({
+        resolver: zodResolver(formSchema),
+        defaultValues: {
+            username: "",
+        },
+    })
+
+    // 2. Define a submit handler.
+    const onSubmit = async (values: z.infer<typeof formSchema>) => {
+        console.log(values)
+    }
+    <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+            <FormField
+                control={form.control}
+                name="username"
+                render={({ field }) => (
+                    <FormItem>
+                        <FormLabel>Username</FormLabel>
+                        <FormControl>
+                            <Input placeholder="shadcn" {...field} />
+                        </FormControl>
+                        <FormDescription>
+                            This is your public display name.
+                        </FormDescription>
+                        <FormMessage />
+                    </FormItem>
+                )}
+            />
+            <Button type="submit">Submit</Button>
+        </form>
+    </Form>
+    return <div>AuthForm</div>
 }
 
 export default AuthForm
