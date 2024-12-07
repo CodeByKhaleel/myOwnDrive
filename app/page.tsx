b2ecc91615acb762f076990ad03253e5a1d73931
khaleelullah <khaleelullah@edvoy.com>
2024-12-07 10:30:30 +0530

    added tailwindcss-animate package and changed css

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div>
-      <h1 className="text-3xl">PreserveIt - The only place to store for free</h1>
+      <h1 className="text-3xl text-brand">PreserveIt - The only place to store for free</h1>
     </div>
   );
 }
