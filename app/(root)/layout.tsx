d70365c60e4083ebe488f62486b04cf89b17e9b1
mohammed khaleelullah <khaleelullah@edvoy.com>
2024-12-14 22:33:44 +0530

    implemented to get currentuser cookies

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -2,10 +2,14 @@ import React from 'react'
 import Sidebar from '@/components/Sidebar'
 import MobileNavigation from '@/components/MobileNavigation'
 import Header from '@/components/Header'
-const Layout = ({children}: {children:React.ReactNode}) => {
+import { getCurrentUser } from '@/lib/actions/user.action'
+import { redirect } from 'next/navigation'
+const Layout = async ({children}: {children:React.ReactNode}) => {
+  const currentUser = await getCurrentUser()
+  if(!currentUser) return redirect('/sign-in')
   return (
     <main className='flex h-screen'>
-     <Sidebar />
+     <Sidebar {...currentUser}/>
       <section className='flex h-full flex-1 flex-col'>
         <MobileNavigation /> <Header />
         <div className='main-content'>{children}
