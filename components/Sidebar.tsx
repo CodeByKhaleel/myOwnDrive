ed490c7c10beeae3a32f40983d870e1489536fbf
mohammed khaleelullah <khaleelullah@edvoy.com>
2024-12-14 22:34:06 +0530

    sidebar image and user

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -5,7 +5,13 @@ import Link from 'next/link'
 import { usePathname } from 'next/navigation'
 import React from 'react'
 import { cn } from "@/lib/utils";
-const Sidebar = () => {
+
+interface Props{
+    fullName:string,
+    avatar:string,
+    email:string
+}
+const Sidebar = ({fullName,avatar,email}:Props) => {
     const pathname = usePathname()
     return (
         <aside className='sidebar'>
@@ -28,21 +34,23 @@ const Sidebar = () => {
             <nav className="sidebar-nav">
                 <ul className="flex flex-1 flex-col gap-6">
                     {navItems.map(({ url, name, icon }) => (
-                        <li key={name} className="lg:w-full">
-                            <Link
-                                href={url}
-                                className={cn(
-                                    "sidebar-nav-item flex items-center gap-3", // Ensures vertical alignment and spacing
-                                    pathname === url && "shad-active"
-                                )}
-                            >
-                                <Image src={icon} alt={name} width={24} height={24} />
-                                <p className="text-base font-medium">{name}</p>
-                            </Link>
+                      <Link key={name} href={url} className='lg:w-full'>
+                        <li className={cn('sidebar-nav-item',pathname === url && 'shad-active')}>
+                        <Image src={icon} alt={name} width={24} height={24} className={cn('nav-icon',pathname === url && 'nav-icon-active')} />
+                        <p className='hidden lg:block'>{name}</p>
                         </li>
+                      </Link>
                     ))}
                 </ul>
             </nav>
+            <Image src="/assets/images/files-2.png" alt="logo" width={506} height={418} className="w-full" />
+            <div className='sidebar-user-info'>
+                <Image src='https://cdn.pixabay.com/photo/2016/08/08/09/17/avatar-1577909_960_720.png' alt="Avatar" width={44} height={44} className='sidebar-user-avatar'/>
+                <div className='hidden lg:block'>
+                    <p className='subtitle-2 capitalize'>{fullName}</p>
+                    <p className='caption'>{email}</p>
+                </div>
+            </div>
         </aside>
     )
 }
