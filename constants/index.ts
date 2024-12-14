93719353db38fc681f0d4b0f7ccea9e73b7719df
mohammed khaleelullah <khaleelullah@edvoy.com>
2024-12-14 09:17:08 +0530

    implementing routes

---
+++ constants/index.ts
@@ -0,0 +1,7 @@
+export const navItems = [
+    {name:'Dashboard', icon:"/assests/icons/dashboard.svg", url:"/"},
+    {name:'Dashboard', icon:"/assests/icons/dashboard.svg", url:"/"},
+    {name:'Dashboard', icon:"/assests/icons/dashboard.svg", url:"/"},
+    {name:'Dashboard', icon:"/assests/icons/dashboard.svg", url:"/"},
+    {name:'Dashboard', icon:"/assests/icons/dashboard.svg", url:"/"},
+]
