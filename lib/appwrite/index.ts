2f30fbc9f819fef20057dee7399b93a36a2aacdd
mohammed khaleelullah <khaleelullah@edvoy.com>
2024-12-14 22:35:17 +0530

    cookies value fixed

--- lib/appwrite/index.ts
+++ lib/appwrite/index.ts
@@ -10,7 +10,7 @@ export const createSessionClient = async () => {
         .setEndpoint(appwriteConfig.endpointUrl)
         .setProject(appwriteConfig.projectId)
 
-    const session = (await cookies()).get('session')    
+    const session = (await cookies()).get('appwrite-session')    
     if(!session || !session.value) throw new Error('No session found')
     
     client.setSession(session.value)
