b552da8ed2c7c20cd12ecd438b1f06e01ae6b439
mohammed khaleelullah <khaleelullah@edvoy.com>
2024-12-14 08:58:34 +0530

    changed directory

--- app/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function Home() {
-  return (
-    <div className="flex-center h-screen">
-      <h1 className="h1">PreserveIt - The only place to store for free</h1>
-    </div>
-  );
-}
