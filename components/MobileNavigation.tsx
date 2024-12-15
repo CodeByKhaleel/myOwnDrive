46c8fdcf13df7027c70fdd2e5e36de7b6732a7d6
mohammed khaleelullah <khaleelullah@edvoy.com>
2024-12-15 17:40:30 +0530

    frontend ui for mobile devices sidebar

--- components/MobileNavigation.tsx
+++ components/MobileNavigation.tsx
@@ -1,11 +1,117 @@
-import React from 'react'
+"use client";
+import React, { useState } from "react";
+import Image from "next/image";
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet";
+import { usePathname } from "next/navigation";
+import { Separator } from "@/components/ui/separator";
+import { navItems } from "@/constants";
+import Link from "next/link";
+import { cn } from "@/lib/utils";
+import { Button } from "./ui/button";
+import FileUploader from '@/components/FileUploder'
 
-const MobileNavigation = () => {
-  return (
-    <div>
-      hiii
-    </div>
-  )
+interface Props {
+  ownerId: string;
+  accountId: string;
+  fullName: string;
+  email: string;
+  avatar: string;
 }
 
-export default MobileNavigation
+const MobileNavigation: React.FC<Props> = ({ ownerId, accountId, fullName, email, avatar }) => {
+  const [open, setOpen] = useState(false);
+  const pathname = usePathname();
+
+  return (
+    <header className="mobile-header">
+      <Image
+        src="/assets/icons/logo-full-brand.svg"
+        alt="logo"
+        width={120}
+        height={52}
+        className="h-auto"
+      />
+      <Sheet open={open} onOpenChange={setOpen}>
+        <SheetTrigger>
+          <Image src="/assets/icons/menu.svg" alt="Menu" width={30} height={30} />
+        </SheetTrigger>
+        <SheetContent className="shad-sheet h-screen px-3">
+          <SheetTitle>
+            <div className="header-user">
+              <Image
+                src={avatar}
+                alt="avatar"
+                width={44}
+                height={44}
+                className="header-user-avatar"
+              />
+              <div className="sm:hidden lg:block">
+                <p className="subtitle-2 capitalize">{fullName}</p>
+                <p className="caption">{email}</p>
+              </div>
+            </div>
+            <Separator className="mb-4 bg-light-200/20" />
+          </SheetTitle>
+          <nav className="mobile-nav">
+            <ul className="mobile-nav-list">
+              {navItems.map(({ url, name, icon }) => (
+                <Link key={name} href={url} className="lg:w-full">
+                  <li
+                    className={cn(
+                      "sidebar-nav-item",
+                      pathname === url && "shad-active"
+                    )}
+                  >
+                    <Image
+                      src={icon}
+                      alt={name}
+                      width={24}
+                      height={24}
+                      className={cn(
+                        "nav-icon",
+                        pathname === url && "nav-icon-active"
+                      )}
+                    />
+                    <p>{name}</p>
+                  </li>
+                </Link>
+              ))}
+            </ul>
+          </nav>
+          <Separator className="my-5 bg-light-200/20" />
+          <div className="flex flex-col justify-between gap-5 pb-5">
+            <FileUploader />
+            <Button
+              type="button"
+              className="mobile-sign-out-button"
+              onClick={() => {
+                console.log("Log out action");
+              }}
+            >
+              <Image
+                src="/assets/icons/logout.svg"
+                alt="logout"
+                width={24}
+                height={24}
+              />
+              <p>Log Out</p>
+            </Button>
+          </div>
+          <SheetDescription>
+            This action cannot be undone. This will permanently delete your
+            account and remove your data from our servers.
+          </SheetDescription>
+        </SheetContent>
+      </Sheet>
+    </header>
+  );
+};
+
+export default MobileNavigation;
